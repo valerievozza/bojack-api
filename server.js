d38592a9023f344e172bc97bbb943adee179ac58
Emanuel Rico Conde <ricocondee@gmail.com>
2022-11-21 17:23:00 -0500

    new route /api added

--- server.js
+++ server.js
@@ -2,20 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const db = require('./db/db')
 const query = require('./db/query')
+const presetData = require('./presetData')
 const app = express()
 const PORT = 8000
 
 // json data has been removed and changed to a sqlite3 database
 
-const unknown = {
-  name: "unknown",
-  birthDate: "unknown",
-  birthPlace: "unknown",
-  species: "unknown",
-  voiceActor: "unknown",
-  image: "unknown",
-  quote: "unknown",
-};
 
 const episodes = {}
 
@@ -29,6 +21,10 @@ app.get('/', (req, res) => {
 })
 
 // Characters JSON Object
+
+app.get('/api',(req,res) => {
+  res.json(presetData.endPoints)
+})
 
 app.get('/api/characters',(req,res) => {
     db.all(query.SELECTALL, (error, data) => {
@@ -48,12 +44,11 @@ app.get('/api/characters/name/:name',(req,res) => {
         (error, data) => {
           if (error) {
             console.log(error.message);
-            return;
           } else if (data.length > 0) {
             res.json(data);
             return;
           }
-          res.json(unknown);
+          res.json(presetData.unknown);
         }
       );
 })
@@ -65,12 +60,11 @@ app.get("/api/characters/:id", (req, res) => {
     db.all(query.SELECTBYID, [characterId], (error, data) => {
       if (error) {
         console.log(error.message);
-        return;
       } else if (data.length > 0) {
         res.json(data);
         return;
       }
-      res.json(unknown);
+      res.json(presetData.unknown);
     });
 });
 
